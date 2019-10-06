47470ef5634f762e7b764b3d85ef5c0ba29f1796
PJ Frias <pj@SmartLogic.io>
2019-10-05 22:34:47 -0400

    select greeting via picker

--- App.js
+++ App.js
@@ -1,36 +1,39 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, Picker } from 'react-native';
 
 const GREETINGS = ['Hello', 'Hi', 'Howdy', 'Salutations', 'Sup'];
 
 export default class App extends Component {
   state = {
     greeting: GREETINGS[0]
-  }
+  };
 
   handleButtonPress = () => {
     const index = GREETINGS.indexOf(this.state.greeting);
     if (index < GREETINGS.length - 1) {
       this.setState({ greeting: GREETINGS[index + 1] });
     } else {
-      this.setState({ greeting: GREETINGS[0]});
+      this.setState({ greeting: GREETINGS[0] });
     }
-  }
+  };
 
   render() {
     const { greeting } = this.state;
     return (
       <View style={styles.container}>
         <Text style={styles.title}>{greeting}, world!</Text>
-        <TouchableOpacity
-          onPress={this.handleButtonPress}
-          style={styles.button}>
-          <Text style={styles.buttonText}>Switch it up</Text>
-        </TouchableOpacity>
+        <Picker
+          selectedValue={greeting}
+          style={styles.picker}
+          onValueChange={(itemValue, itemIndex) => this.setState({ greeting: itemValue })}>
+          {GREETINGS.map(g => {
+            return <Picker.Item key={g} label={g} value={g} />
+          })}
+        </Picker>
       </View>
     );
   }
-}
+};
 
 
 const styles = StyleSheet.create({
@@ -46,11 +49,9 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 24,
   },
-  button: {
-    borderRadius: 5,
-    borderColor: 'gray',
-    borderWidth: 1,
+  picker: {
     margin: 45,
     padding: 15,
+    width: 200,
   }
 });
