e48d4284feb293d53b37e479b510eda1106701fe
PJ Frias <pj@SmartLogic.io>
2019-10-05 23:32:03 -0400

    move container styling to stylesheet

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Text, View, StyleSheet } from 'react-native';
 export default class App extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={styles.container}>
         <Text style={styles.title}>Hello, world!</Text>
       </View>
     );
@@ -13,6 +13,11 @@ export default class App extends Component {
 
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
   title: {
     fontSize: 24,
     fontWeight: '600',
