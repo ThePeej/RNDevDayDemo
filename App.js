36839f6285324d08606774a2a82328fbe3bc5778
PJ Frias <pj@SmartLogic.io>
2019-10-05 21:52:56 -0400

    list all greetings at once

--- App.js
+++ App.js
@@ -4,29 +4,12 @@ import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 const GREETINGS = ['Hello', 'Hi', 'Howdy', 'Salutations', 'Sup'];
 
 export default class App extends Component {
-  state = {
-    greeting: GREETINGS[0]
-  }
-
-  handleButtonPress = () => {
-    const index = GREETINGS.indexOf(this.state.greeting);
-    if (index < GREETINGS.length - 1) {
-      this.setState({ greeting: GREETINGS[index + 1] });
-    } else {
-      this.setState({ greeting: GREETINGS[0]});
-    }
-  }
-
   render() {
-    const { greeting } = this.state;
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>{greeting}, world!</Text>
-        <TouchableOpacity
-          onPress={this.handleButtonPress}
-          style={styles.button}>
-          <Text style={styles.buttonText}>Switch it up</Text>
-        </TouchableOpacity>
+        {GREETINGS.map(greeting => {
+          return <Text key={greeting} style={styles.title}>{greeting}, world!</Text>
+        })}
       </View>
     );
   }
