aa35d18bee2a8b6cbc45c321dda2fe83a89ca5c4
PJ Frias <pj@SmartLogic.io>
2019-10-05 21:44:42 -0400

    loop over greetings via button

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+
+const GREETINGS = ['Hello', 'Hi', 'Howdy', 'Salutations', 'Sup'];
 
 export default class App extends Component {
+  state = {
+    greeting: GREETINGS[0]
+  }
+
+  handleButtonPress = () => {
+    const index = GREETINGS.indexOf(this.state.greeting);
+    if (index < GREETINGS.length - 1) {
+      this.setState({ greeting: GREETINGS[index + 1] });
+    } else {
+      this.setState({ greeting: GREETINGS[0]});
+    }
+  }
+
   render() {
+    const { greeting } = this.state;
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text style={styles.title}>Hello, world!</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>{greeting}, world!</Text>
+        <TouchableOpacity
+          onPress={this.handleButtonPress}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Switch it up</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -13,8 +34,23 @@ export default class App extends Component {
 
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
   title: {
-    fontSize: 24,
+    fontSize: 36,
     fontWeight: '600',
   },
+  buttonText: {
+    fontSize: 24,
+  },
+  button: {
+    borderRadius: 5,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 45,
+    padding: 15,
+  }
 });
